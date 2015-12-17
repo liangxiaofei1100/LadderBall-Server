17ac3ba7cd213b3a2865cb893ab1f977b6c61581
liangxiaofei <liangxiaofei1100@163.com>
2015-12-17 10:11:31 +0800

    管理平台小节事件接口

---
+++ ladderball_java/management/src/main/java/com/zhaoyan/ladderball/management/event/EventController.java
@@ -0,0 +1,66 @@
+package com.zhaoyan.ladderball.management.event;
+
+import com.zhaoyan.ladderball.domain.eventofmatch.http.*;
+import com.zhaoyan.ladderball.service.event.EventService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/match")
+public class EventController {
+    Logger logger = LoggerFactory.getLogger(EventController.class);
+
+    @Autowired
+    EventService eventService;
+
+    @RequestMapping(value = "/event/add", method = RequestMethod.POST, produces = {"application/json;charset=UTF-8"})
+    public
+    @ResponseBody
+    EventCollectionResponse addEvent(@RequestBody EventCollectionRequest request) {
+        logger.debug("addEvent() EventCollectionRequest: " + request);
+        EventCollectionResponse response = eventService.addEvent(request);
+        return response;
+    }
+
+    /**
+     * 获取小节事件
+     */
+    @RequestMapping(value = "/event/partlist", method = RequestMethod.POST, produces = {"application/json;charset=UTF-8"})
+    public
+    @ResponseBody
+    EventPartListResponse getPartEvent(@RequestBody EventPartListRequest request) {
+        logger.debug("getPartEvent() EventPartListRequest: " + request);
+        EventPartListResponse response = eventService.getPartEvent(request);
+        return response;
+    }
+
+    /**
+     * 删除一个事件
+     */
+    @RequestMapping(value = "/event/delete", method = RequestMethod.POST, produces = {"application/json;charset=UTF-8"})
+    public
+    @ResponseBody
+    EventDeleteResponse deleteEvent(@RequestBody EventDeleteRequest request) {
+        logger.debug("deleteEvent() EventDeleteRequest: " + request);
+        EventDeleteResponse response = eventService.deleteEvent(request);
+        return response;
+    }
+
+    /**
+     * 修改一个事件
+     */
+    @RequestMapping(value = "/event/modify", method = RequestMethod.POST, produces = {"application/json;charset=UTF-8"})
+    public
+    @ResponseBody
+    EventModifyResponse modifyEvent(@RequestBody EventModifyRequest request) {
+        logger.debug("modifyEvent() EventModifyRequest: " + request);
+        EventModifyResponse response = eventService.modifyEvent(request);
+        return response;
+    }
+}
